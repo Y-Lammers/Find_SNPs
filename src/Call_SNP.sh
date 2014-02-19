e49b9f544f158884950350de5e596ca9280fa755
Y-Lammers <youri_lammers_88@hotmail.com>
2014-02-19 14:30:07 +0100

    Now supports parallel SNP / Base searching. Todo: fix samtools / bcftools paths.

--- src/Call_SNP.sh
+++ src/Call_SNP.sh
@@ -3,13 +3,13 @@
 # Script arguments: [reference] [output_dir] [bam_file]
 
 # get variables
-reference_file=$1
+reference=$1
 output_directory=$2
 bam=$3
-base=$output_directory${bam%.*}
+base=$output_directory$(echo ${bam%.*} | sed 's/.*\///g')
 
 # get variation + filter
-samtools mpileup -uf $reference_file -E -L 10000 -d 10000 $bam | bcftools view -bvcg - > $base".bcf"
-bcftools view $base".bcf" | /usr/share/samtools/vcfutils.pl varFilter -w 0 -W 0 -a 1 | awk '{if (/^#/ || $6>60) print }' > $base".vcf"
+/usr/local/src/samtools-0.1.19/samtools mpileup -uf $reference -E -L 10000 -d 10000 $bam | /usr/local/src/samtools-0.1.19/bcftools/bcftools view -bvcg - > $base".bcf"
+/usr/local/src/samtools-0.1.19/bcftools/bcftools view $base".bcf" | /usr/local/src/samtools-0.1.19/bcftools/vcfutils.pl varFilter -w 0 -W 0 -a 1 | awk '{if (/^#/ || $6>60) print }' > $base".vcf"
 
 cat $base".vcf" >> $output_directory"variantion.vcf"
--- src/Retrieve_SNP_Positions.sh
+++ src/Retrieve_SNP_Positions.sh
@@ -3,24 +3,27 @@
 # Script arguments: [reference] [output_dir] [sample1.bam] [sample2.bam]
 
 # get and set variables
-reference_file=$1
+reference=$1
 output_directory=${2%*/}/
 sample1=$3
 sample2=$4
-bases1 = $output_directory$(echo ${sample1%".bam"} | sed 's/.*\///g')"_snp_positions.txt"
-bases2 = $output_directory$(echo ${sample2%".bam"} | sed 's/.*\///g')"_snp_positions.txt"
-SNP_output = $output_directory"Usuable_SNPs.tsv"
+bases1=$output_directory$(echo ${sample1%".bam"} | sed 's/.*\///g')"_snp_positions.txt"
+bases2=$output_directory$(echo ${sample2%".bam"} | sed 's/.*\///g')"_snp_positions.txt"
+SNP_output=$output_directory"Usuable_SNPs.tsv"
+DIR=$( cd "$( dirname "$0" )" && pwd )/
 
 # Call SNPs for both samples
-Call_SNP.sh $reference $output_directory $sample1
-Call_SNP.sh $reference $output_directory $sample2
+${DIR}Call_SNP.sh $reference $output_directory $sample1 &
+${DIR}Call_SNP.sh $reference $output_directory $sample2 &
+wait
 
 # Remove duplicate SNP positions
 grep "^[^#]" $output_directory"variantion.vcf" | cut -f1-5 | sort --unique >> $output_directory"unique_variation.txt"
 
 # Obtain bases for each SNP position
-Call_Base.sh $reference $output_directory $sample1
-Call_Base.sh $reference $output_directory $sample2
+${DIR}Call_Base.sh $reference $output_directory $sample1 &
+${DIR}Call_Base.sh $reference $output_directory $sample2 &
+wait
 
 # Call the Parse_Bases python script to produce the viable SNP positions file
-Parse_Bases.py $bases1 $bases2 $SNP_output
+${DIR}Parse_Bases.py $bases1 $bases2 $SNP_output
